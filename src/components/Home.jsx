db22da6c0484bd6acde8ded6edb3f0d423dfea69
Dikshant <dikshantnaudiyal06@gmail.com>
2023-04-11 16:05:40 +0530

    My portfolio complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/home.css";
 import profile from "../assets/mainImg.jpg";
-import cv from "../assets/FullStack Developer Dikshant Resume.pdf"
 
 function Home() {
   const click= ()=>{
